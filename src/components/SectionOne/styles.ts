7fbe3ab560c47e76c063701815342e0993f3f36c
phmtsweb <phmtsweb@gmail.com>
2022-05-26 09:27:20 -0300

    Updates

--- src/components/SectionOne/styles.ts
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-export const Section = styled.section`
-  background-image: url(/background.png);
-  background-size: cover;
-  padding-top: 5.7rem;
-`;
--- src/components/SectionTwo/styles.ts
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-export const Section = styled.section`
-  max-width: 144rem;
-  width: 100%;
-  margin: 0 auto;
-`;
